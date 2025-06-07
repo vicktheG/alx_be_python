4f15987cc6b68956e431c0267989f0808092b1d4
victheG <victoronyangooketch1@gmail.com>
2025-06-07 12:24:24 +0300

    Update arithmetic module

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,21 @@
 def perform_operation(num1, num2, operation):
     match operation:
         case "add":
+            result = num1 + num2
             print(num1 + num2)
         case "subtract":
+            result = num1 - num2
             print(num1 - num2)
         case "multiply":
+            result = num1 * num2
             print(num1 * num2)
         case "divide":
+            result = num1 / num2
             if num2 == 0:
                 print("Cannot be divide by 0")
             else:
                 print(num1 / num2)
         case _:
             print("Enter a valid operand")
+
+    return result
