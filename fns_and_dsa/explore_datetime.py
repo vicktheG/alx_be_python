185bc32d08fa9cc9f68e51ff247bdff86a0191ca
victheG <victoronyangooketch1@gmail.com>
2025-06-08 17:33:00 +0300

    Add future date script

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,26 @@
+import datetime
+
+
+# Function to print current time in YYYY MM DD HH MM SS
+def display_current_datetime():
+    current_date = datetime.datetime.today()
+    # to remove the microseconds
+    dt = current_date.replace(microsecond=0)
+    print(f"Current date and time: {dt} ")
+
+
+display_current_datetime()
+
+
+# function to calculate a future date
+def calculate_future_date():
+    days = int(input("Enter the number of days to add to the current date: "))
+    dt = datetime.datetime.today()
+    future = datetime.timedelta(days)
+    future_date = dt + future
+    future_date = future_date.replace(microsecond=0)
+
+    print(f"Future date: {future_date}")
+
+
+calculate_future_date()
